8df8b33e420eb6282a78f9260cbbb40a00cb8191
igorzakhar <igorzakhar@gmail.com>
2017-04-23 00:47:16 +0300

    Add solution

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # Github Trends
 
-[TODO. There will be project description]
+Данная программа находит 20 репозиоториев на GitHub, созданных за последнюю неделю, которые набрали наибольшее количество звезд.
 
-# Project Goals
+# Как использовать
+Программа использует стороннюю библиотеку [requests](https://github.com/kennethreitz/requests)
+Библиотек requests можно установить через файл зависимостей приложения requirements.txt:
+```#!bash
+$ pip3 install -r requirements.txt
+```
+Рекомендуется устанавливать зависимости в виртуальном окружении используя [virtualenv](https://github.com/pypa/virtualenv), [virtualenvwrapper](https://pypi.python.org/pypi/virtualenvwrapper).
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Пример запуска в Linux, Python 3.5.2:
+```#!bash
+$ python github_trending.py
+```
+Пример вывода результатов:
+```#!bash
+-------------------------------------------------------------------------------------------------------
+             Name              | Stars | Open issues | URL
+-------------------------------------------------------------------------------------------------------
+pytorch-CycleGAN-and-pix2pix   | 1175  |      5      | https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+-------------------------------------------------------------------------------------------------------
+tamperchrome                   |  508  |      1      | https://github.com/google/tamperchrome
+-------------------------------------------------------------------------------------------------------
+SuperTextView                  |  483  |      2      | https://github.com/chenBingX/SuperTextView
+-------------------------------------------------------------------------------------------------------
+fastText_multilingual          |  370  |      1      | https://github.com/Babylonpartners/fastText_multilingual
+-------------------------------------------------------------------------------------------------------
+polaris                        |  348  |      6      | https://github.com/Shopify/polaris
+-------------------------------------------------------------------------------------------------------
+```
+
+# Цели проекта
+
+Код написан для образовательных целей. Учебный курс для веб-разработчиков - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -3,38 +3,53 @@ from datetime import date, timedelta
 import requests
 
 
-BASE_URL = 'https://api.github.com/search/repositories'
+BASE_URL = 'https://api.github.com'
 TOP_SIZE = 20
-RECENT_DAYS = 1
+RECENT_DAYS = 7
 
 
 def get_date(recent_days):
     return date.today() - timedelta(days=recent_days)
 
 
-def get_trending_repositories(top_size, ):
-    query_params = {'q': 'created'.format(recent_days),
-                    'sort': 'stars',
-                    'per_page': top_size}
-
-    
-
-
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
-
-if __name__ == '__main__':
+def get_trending_repositories(top_size):
+    url = BASE_URL +'/search/repositories'
     query_params = {'q': 'created:>={}'.format(get_date(RECENT_DAYS)),
                     'sort': 'stars',
-                    'per_page': TOP_SIZE}
-    response = requests.get(BASE_URL, params=query_params)
-    #print(get_date(RECENT_DAYS))
-    print(response.url)
+                    'per_page': top_size}
+    response = requests.get(url, params=query_params)
     response_dict = response.json()
-    resp_dicts = response_dict['items']
-    for resp_dict in resp_dicts[:20]:
-        print(resp_dict['name'])
+    repositories_dict = response_dict['items']
+    return repositories_dict
+    
+
+def get_open_issues_amount(repo_owner, repo_name):
+    url = BASE_URL + '/repos/{}/{}/issues'.format(repo_owner, repo_name)
+    response = requests.get(url)
+    response_dict = response.json()
+    open_issues = len(response_dict)
+    return open_issues, response.headers['X-RateLimit-Remaining']
 
 
+def pretty_print_output(repositories):
+    header_string = '{0:^30} | {1:^5} | {2:^11} | {3:<45}'
+    format_string = '{0:<30} | {1:^5} | {2:^11} | {3:<45}'
+    separator_string = '-' * 103
+    print(separator_string)
+    print(header_string.format('Name', 'Stars', 'Open issues', 'URL'))
+    print(separator_string)
+    for repo in repositories:
+        open_issues, rate_limit = get_open_issues_amount(
+                                          repo['owner']['login'], repo['name'])
+        if rate_limit == '0':
+            open_issues = repo['open_issues']
+        print(format_string.format(repo['name'], repo['stargazers_count'],
+                                   open_issues,
+                                   repo['html_url']))
+        print(separator_string)
 
+
+if __name__ == '__main__':
+    trending_repositories = get_trending_repositories(TOP_SIZE)
+    pretty_print_output(trending_repositories)
 
